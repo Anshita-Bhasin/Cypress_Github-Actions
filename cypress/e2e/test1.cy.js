f0355b669f2cf1ff9257f31139c0deed829fb9b3
Anshita Bhasin <anshita.bhasin@gmail.com>
2022-12-01 15:32:46 +0400

    Cypress Test Case

--- cypress/e2e/test1.cy.js
+++
@@ -1,7 +0,0 @@
-describe('Testing through Cypress',()=>{
-
-    it('Open website and enter username, password',()=>{
-
-        cy.visit('https://ecommerce-playground.lambdatest.io/index.php?route=account/login')
-    })
-})
