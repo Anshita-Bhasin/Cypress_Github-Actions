cb5d082fbaf897fbde8589d0aedbd611b2b83a7a
Anshita Bhasin <anshita.bhasin@gmail.com>
2023-02-12 18:15:07 +0400

    update docker conatiner

--- cypress/e2e/HomePage/homePage.cy.js
+++ cypress/e2e/HomePage/homePage.cy.js
@@ -1,4 +1,9 @@
 describe("Automation using Cypress", () => {
+  beforeEach(function () {
+    cy.visit(
+      "https://ecommerce-playground.lambdatest.io/index.php?route=account/login"
+    );
+  });
   it("Open website and enter username, password", () => {
     cy.visit(
       "https://ecommerce-playground.lambdatest.io/index.php?route=account/login"
